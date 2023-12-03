19bbbaba977d1b6802fdb13aff2acba6466d73f8
royperalta <rrpb33@gmail.com>
2023-12-03 00:50:27 -0500

    mejoras en el diseño

--- src/app/biblia/catecismo/PopUp.jsx
+++ src/app/biblia/catecismo/PopUp.jsx
@@ -1,14 +1,17 @@
 import Style from './PopUp.module.css'
+import { IoMdCloseCircle } from "react-icons/io";
 
-const PopUp = ({onClose,children}) =>{
+const PopUp = ({onClose,color,children}) =>{
     
     return(
         <div className={Style.popup}>
-            <div className={Style.popUpContent}>                
-                {children}
-                <button onClick={onClose} className={Style.closeBtn}>
-                    X
-                </button>
+            <div className={`${Style.popUpContent} ${color ? 'bg-green-400':'bg-white'}`}>                
+                {children}              
+
+               
+                {color ? <button
+                 className='bg-white p-3 rounded-md block w-full'
+                 onClick={onClose}>OK</button>: <IoMdCloseCircle onClick={onClose} className={Style.closeBtn} />}
             </div>
         </div>
     )
--- src/app/biblia/catecismo/page.jsx
+++ src/app/biblia/catecismo/page.jsx
@@ -1,8 +1,9 @@
 'use client'
 import React, { useEffect, useRef, useState } from 'react'
+import { GrNext, GrPrevious } from "react-icons/gr";
 import {
   AiOutlineSound,
-  AiFillSound
+  AiFillSound,
 } from "react-icons/ai";
 import PopUp from './PopUp';
 
@@ -12,12 +13,12 @@ export default function PageCatecismo() {
     {
       id: 1,
       question: "What is the chief end of man?",
-      answer: "Man’s chief end is to glorify God, and to enjoy him for ever"
+      answer: "Man's chief end is to glorify God, and to enjoy him forever"
     },
     {
       id: 2,
       question: "What rule hath God given to direct us how we may glorify and enjoy him?",
-      answer: "The Word of God, which is contained in the Scriptures of the Old and New Testaments, is the only rule to direct us how we may glorify and enjoy him. "
+      answer: "The Word of God, which is contained in the Scriptures of the Old and New Testaments, is the only rule to direct us how we may glorify and enjoy him"
     },
     {
       id: 3,
@@ -32,6 +33,9 @@ export default function PageCatecismo() {
   const [numberQuestion, setNumberQuestion] = useState(0)
   const [respuesta, setRespuesta] = useState("")
   const [verRespuesta, setVerRespuesta] = useState(false)
+  const [validacionRespuesta, setValidacionRespuesta] = useState(false)
+
+  const [error,setError] = useState(false)
 
 
   let audioRef = useRef()
@@ -42,17 +46,15 @@ export default function PageCatecismo() {
 
   const handleNext = () => {
     if (numberQuestion < PREGUNTAS.length - 1) {
+      document.getElementsByTagName("textarea")[0].value = ""
       setNumberQuestion(numberQuestion + 1)
-    } else {
-      alert('No hay mas preguntas')
     }
   }
 
   const handlePrev = () => {
     if (numberQuestion > 0) {
+      document.getElementsByTagName("textarea")[0].value = ""
       setNumberQuestion(numberQuestion - 1)
-    } else {
-      alert('No hay mas preguntas')
     }
   }
 
@@ -62,22 +64,23 @@ export default function PageCatecismo() {
 
   const handleCloseRespuesta = () => {
     setVerRespuesta(false)
+    setValidacionRespuesta(false)
+    setError(false)
   }
 
   const handleRespuesta = (event) => {
+    console.log(event.target.value)
     setRespuesta(event.target.value)
   }
   const handleCheck = () => {
 
-    if (respuesta.includes(PREGUNTAS[numberQuestion].answer)) {
-      alert('Correcto')
+    if (respuesta.includes(PREGUNTAS[numberQuestion].answer) && respuesta !== '') {
+      setValidacionRespuesta(true)
     } else {
-      alert('Intente nuevamente')
+      setError(true)
     }
 
   }
-
-
 
   const handleAudioQuestion = () => {
     audioRef.current.src = `/audio/${numberQuestion + 1}q.mp3`
@@ -90,10 +93,10 @@ export default function PageCatecismo() {
   }
 
   return (
-    <div className='w-full bg-[#634339] h-screen text-white flex flex-col justify-center items-center'>
-      <div className='w-screen h-screen flex justify-center flex-col'>
-        <div className='relative'>
-          <h2 className='h-10 text-center'>Question {PREGUNTAS[numberQuestion].id}</h2>
+    <div className='w-full bg-[#634339] h-screen text-white flex flex-col '>
+      <div className='w-screen h-screen flex flex-col'>
+        <div className='flex flex-col'>
+          <h2 className='h-5 p-7 text-center'>Question {PREGUNTAS[numberQuestion].id}</h2>
           <div className='h-f bg-[#634339] flex flex-col justify-center items-center'>
             <div
               onClick={handleAudioQuestion}
@@ -107,31 +110,47 @@ export default function PageCatecismo() {
             </div>
             <div>
               <textarea
-                className='w-96 h-40 p-4 text-black text-2xl rounded-md'
+                className='w-96 h-56 p-4 text-black text-2xl rounded-md border-[#e3c7be]'
                 type="text"
                 onChange={handleRespuesta}
+                placeholder='Type the answer...'
               />
             </div>
-            <div>
+            <div className='p-3'>
               <button
+                className='bg-[#b07a69] p-2 rounded-md'
                 onClick={handleCheck}
               >Check</button>
             </div>
-            <div>
-              <button onClick={handleVerRespuesta} >{verRespuesta ? '' : 'Ver respuesta'}</button>
-            </div>
+
             <div className='absolute bottom-0'>
               {verRespuesta && (
                 <PopUp onClose={handleCloseRespuesta}>
                   {PREGUNTAS[numberQuestion].answer}
                 </PopUp>
               )}
-            </div>
-          </div>
 
-          <div className='flex justify-between items-center gap-10 bottom-20'>
-            <button onClick={handlePrev} className='border-[#8c5949] bg-[#f6edea] text-[#8c5949] rounded-md p-2'>Prev</button>
-            <button onClick={handleNext} className='border-[#8c5949] bg-[#f6edea] text-[#8c5949] rounded-md p-2'>Next</button>
+              {validacionRespuesta && (
+                <PopUp onClose={handleCloseRespuesta} color={true}>
+                  Great job!...
+                </PopUp>
+              )}
+
+              {error && (
+                <PopUp onClose={handleCloseRespuesta} color={false}>
+                  Oops! Try again!...
+                </PopUp>
+              )}
+            </div>
+            <div className='w-96 flex justify-around gap-10 bottom-20 items-center'>
+
+              <GrPrevious onClick={handlePrev} className='w-[40px] h-[40px]' />
+
+              <button onClick={handleVerRespuesta} >{verRespuesta ? '' : 'Show answer'}</button>
+
+              <GrNext onClick={handleNext} className='w-[40px] h-[40px]' />
+
+            </div>
           </div>
         </div>
       </div>
