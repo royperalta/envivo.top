02b3bbb2b9ebe7c9fb487e81f9f421fdfa2009cb
royperalta <rrpb33@gmail.com>
2024-02-15 15:11:35 -0500

    pregunta 4, 5 y 6

--- src/app/istqb/pro/page.jsx
+++ src/app/istqb/pro/page.jsx
@@ -3,6 +3,9 @@ import React, { useEffect, useState } from 'react'
 import JSON1 from './jsons/istqb1.json'
 import JSON2 from './jsons/istqb2.json'
 import JSON3 from './jsons/istqb3.json'
+import JSON4 from './jsons/istqb4.json'
+import JSON5 from './jsons/istqb5.json'
+import JSON6 from './jsons/istqb6.json'
 import Preguntas from './Preguntas'
 export default function Exam() {
 
@@ -14,11 +17,14 @@ export default function Exam() {
   const lista = [
     { id: 1, json: JSON1 },
     { id: 2, json: JSON2 },
-    { id: 3, json: JSON3 }
+    { id: 3, json: JSON3 },
+    { id: 4, json: JSON4 },
+    { id: 5, json: JSON5 },
+    { id: 6, json: JSON6 }
   ]
 
   const fetchData = (number) => {
-    const jsons = [null, JSON1, JSON2, JSON3]
+    const jsons = [null, JSON1, JSON2, JSON3,JSON4,JSON5,JSON6]
     setDatos(jsons[number])
   }
 
@@ -28,7 +34,7 @@ export default function Exam() {
 
   const handleReset = (number) => {
     setDatos([]);
-    const jsons = [null, JSON1, JSON2, JSON3]
+    const jsons = [null, JSON1, JSON2, JSON3,JSON4,JSON5,JSON6]
     setDatos(jsons[number])
     setNumberExam(number)
   }
@@ -38,11 +44,11 @@ export default function Exam() {
     <div className='w-screen h-screen mx-auto lg:w-[850px]'>
       <h1 className='text-center p-2 font-extrabold text-2xl text-[#23242e]'>Seleccione la lista de preguntas</h1>
       <div className='font-extrabold text-center text-4xl text-[#353745]'> EXAMEN {numberExam}</div>
-      <div className='flex'>
+      <div className='grid grid-cols-3 md:grid-cols-7 gap-2 mt-5'>
         {lista.map((examen, index) => (
           <div
             key={index}
-            className='bg-[#6a708d] text-white p-2 m-1 font-bold border rounded-md'
+            className={`${numberExam === (index+1) ? 'bg-[#6a708d] text-white':'bg-gray-200 text-black'} cursor-pointer  p-2 m-1 font-bold border rounded-md`}
             onClick={() => { fetchData(index + 1); handleReset(index + 1) }}
           >
             Examen: {examen.id}
